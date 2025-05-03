1809deaefc832912460e4695dba3cebaf0df063f
ChintanRajpara <rajparachintan08@gmail.com>
2025-05-04 00:13:45 +0530

    added example env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppContextProvider } from "./components/appContextProvider";
 import { AppDialogsContainer } from "./components/appDialogsContainer";
 import { PostListContainer } from "./pages/postList/postListContainer";
-import { PostDetail } from "./pages/postDetail";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   { path: "/", element: <PostListContainer /> },
   { path: "/post/:id", element: <PostDetailContainer /> },
-  { path: "/p/:id", element: <PostDetail /> },
 
   { path: "*", element: <NotFound /> },
 ]);
