a11a0d4914946beb32ec5137a78bb4da581220ed
ChintanRajpara <rajparachintan08@gmail.com>
2025-05-03 20:04:42 +0530

    removed dist

--- api-server/dist/features/comments/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.comments = void 0;
-var comments_route_1 = require("./comments.route");
-Object.defineProperty(exports, "comments", { enumerable: true, get: function () { return __importDefault(comments_route_1).default; } });
